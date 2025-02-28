c8af5e74d6127cc9411483b7122e486b892e6abc
cv2 <kostelgame@gmail.com>
2025-02-28 16:36:13 -0500

    rsync, disabled fs::copy

--- pre-build.sh
+++ pre-build.sh
@@ -1,27 +1,77 @@
 prepare() {
-  echo "no patches found"
+  cd $dendro_buildroot
+  # fix issue with /etc/profile.d/debuginfod.sh for zsh:
+  # https://gitlab.archlinux.org/archlinux/packaging/packages/elfutils/-/issues/2
+  git cherry-pick -n 00cb3efe36337f27925dbff9b2e7d97c7df95bf8
+
+  # remove failing test due to missing glibc debug package during test: https://bugs.archlinux.org/task/74875
+  sed -e 's/run-backtrace-native.sh//g' -i tests/Makefile.am
+
+  autoreconf -fiv
 }
 
 build() {
-  cd "$dendro_buildroot"
-  ./autogen.sh
-  ./configure --prefix=/usr --with-crypto=libgcrypt
+  local configure_options=(
+    --prefix=/usr
+    --sysconfdir=/etc
+    --program-prefix="eu-"
+    --enable-deterministic-archives
+    --enable-maintainer-mode
+  )
+
+  # fat-lto-objects is required for non-mangled .a files in libelf
+  CFLAGS+=" -ffat-lto-objects"
+
+  # debugging information is required for test-suite
+  CFLAGS+=" -g"
+
+  cd $dendro_buildroot
+  ./configure "${configure_options[@]}"
   make
 }
 
-package() {
-  cd "${pkgname}"
-  make DESTDIR="$dendro_installloc" install install_python
+check() {
+  make -C $dendro_buildroot check
+}
 
-  # install bash completion (FS#44618)
-  /usr/bin/install -Dm644 btrfs-completion "${dendro_installloc}/usr/share/bash-completion/completions/btrfs"
+_pick() {
+  local p="$1" f d; shift
+  for f; do
+    d="$dendro_buildroot/$p/${f#$dendro_installloc/}"
+    mkdir -p "$(dirname "$d")"
+    mv "$f" "$d"
+    rmdir -p --ignore-fail-on-non-empty "$(dirname "$f")"
+  done
+}
 
-  # install mkinitcpio hooks
-  cd "${srcdir}"
-  /usr/bin/install -Dm644 initcpio-install-btrfs "${dendro_installloc}/usr/lib/initcpio/install/btrfs"
-  install -Dm644 initcpio-hook-btrfs "${dendro_installloc}/usr/lib/initcpio/hooks/btrfs"
+package_elfutils() {
 
-  # install scrub service/timer
-  /usr/bin/install -Dm644 btrfs-scrub@.service "${dendro_installloc}/usr/lib/systemd/system/btrfs-scrub@.service"
-  /usr/bin/install -Dm644 btrfs-scrub@.timer "${dendro_installloc}/usr/lib/systemd/system/btrfs-scrub@.timer"
+  make DESTDIR="$dendro_installloc" install -C $dendro_buildroot
+
+  # set the default DEBUGINFOD_URLS environment variable to the distribution's debuginfod URL
+  echo "https://debuginfod.archlinux.org" > "$dendro_installloc/etc/debuginfod/archlinux.urls"
+
+  (
+    cd "$dendro_installloc"
+
+    _pick libelf etc/debuginfod
+    _pick libelf etc/profile.d/*
+    _pick libelf usr/{include,lib}
+    _pick libelf usr/share/man/*/{,lib}elf*
+    _pick debuginfod usr/bin/debuginfod*
+    _pick debuginfod usr/share/fish/vendor_conf.d/debuginfod.fish
+    _pick debuginfod usr/share/man/*/debuginfod*
+  )
+
+  install -vDm 644 $dendro_buildroot/{AUTHORS,ChangeLog,NEWS,NOTES,README} -t "$dendro_installloc/usr/share/doc/elfutils/"
+}
+
+package_libelf() {
+  mv -v libelf/* "$dendro_installloc"
+  install -vDm 644 $dendro_buildroot/{AUTHORS,ChangeLog,NEWS,NOTES,README} -t "$dendro_installloc/usr/share/doc/libelf/"
+}
+
+package_debuginfod() {
+  mv -v debuginfod/* "$dendro_installloc"
+  install -vDm 644 $dendro_buildroot/{AUTHORS,ChangeLog,NEWS,NOTES,README} -t "$dendro_installloc/usr/share/doc/debuginfod/"
 }
--- src/buildsystem/handleGitSource.cpp
+++ src/buildsystem/handleGitSource.cpp
@@ -42,7 +42,8 @@ void BuildSystem::handleGitSource(const std::string& sourceUrl,
             throw std::runtime_error("Clone failed: " + repoUrl);
         }
 
-        fs::copy(clonePath, build_root.string(),  fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+        //fs::copy(clonePath, build_root.string(),  fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+        system(("rsync -avL " + clonePath + " " + build_root.string()).c_str());
         fs::remove_all(clonePath);
 
         // Verify signed tag if requested
